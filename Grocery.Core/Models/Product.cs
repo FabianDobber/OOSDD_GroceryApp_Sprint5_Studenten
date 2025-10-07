ee0a1e62a7bd896f6bb8246eb443c06844c20cb2
Fabian <s1214550@student.windesheim.nl>
2025-10-07 11:55:29 +0200

    voeg prijsveld (int) toe aan Product, update repository en view

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -7,13 +7,24 @@ namespace Grocery.Core.Models
         [ObservableProperty]
         public int stock;
         public DateOnly ShelfLife { get; set; }
-        public Product(int id, string name, int stock) : this(id, name, stock, default) { }
+        public object Price { get; }
+        public DateOnly DateOnly { get; }
 
-        public Product(int id, string name, int stock, DateOnly shelfLife) : base(id, name) 
+        public Product(int id, string name, int stock) : this (id, name, stock, default) { }
+
+        public Product(int id, string name, int stock, DateOnly shelfLife, int price) : base(id, name)
         {
             Stock = stock;
             ShelfLife = shelfLife;
+            Price = price;
         }
+
+        public Product(int id, string name, int stock, DateOnly dateOnly) : base(id, name)
+        {
+            this.stock = stock;
+            DateOnly = dateOnly;
+        }
+
         public override string? ToString()
         {
             return $"{Name} - {Stock} op voorraad";
